8dd5fe9f0dcbe5bce9b6a624cd62f159e1902173
btbrad <biantaobrad@gmail.com>
2020-11-25 21:23:46 +0800

    编译文本指令

--- compile.js
+++ compile.js
@@ -12,6 +12,7 @@ class Compile {
     Array.from(childNodes).forEach(node => {
       if (this.isElement(node)) {
         console.log('编译元素节点', node)
+        this.compileElement(node)
       } else if (this.isInter(node)) {
         console.log('编译文本节点', node.textContent)
         this.compileText(node)
@@ -32,5 +33,25 @@ class Compile {
   compileText(node) {
     console.log(RegExp.$1)
     node.textContent = this.$vm[RegExp.$1]
+  }
+
+  compileElement(node) {
+    const nodeAttrs = node.attributes
+    Array.from(nodeAttrs).forEach(attr => {
+      const attrName = attr.name
+      const exp = attr.value
+      if (this.isDirective(attrName)) {
+        const dir = attrName.substring(3)
+        this[dir] && this[dir](node, exp)
+      }
+    })
+  }
+
+  isDirective(name) {
+    return name.indexOf('my') === 0
+  }
+
+  text(node, exp) {
+    node.textContent = this.$vm[exp]
   }
 }
